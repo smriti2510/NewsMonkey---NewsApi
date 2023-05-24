64acca05333f6ed800e33c5a6e6f6a214a65746a
Smriti Agarwal <80616769+smriti2510@users.noreply.github.com>
2023-05-24 19:02:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,25 +3,31 @@ import './App.css';
 import React, { useState } from 'react'
 import NavBar from './Components/NavBar';
 import News from './Components/News';
+import LoadingBar from 'react-top-loading-bar'
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 const App = ()=> {
   const pageSize = 6;
-  const [ setProgress] = useState(0)
+  const [progress, setProgress] = useState(0)
  
-  return (
-    <div>
-      <Router>
-      <NavBar/>
+    return (
+      <div>
+        <Router>
+        <NavBar/> 
+        <LoadingBar
+        height={3}
+        color='#f11946'
+        progress={progress} 
+      />
       <Routes>
-        <Route path="/" element={< News  key='General' pageSize={pageSize} country="in" setProgress={setProgress} category="General"/>}></Route>
-        <Route path="/Business" element={ <News key='Business'  pageSize={pageSize} country="in" category="Business"/>}></Route>
-        <Route path="/Entertainment" element={<News key='Entertainment'  pageSize={pageSize} country="in" category="Entertainment"/>}></Route>
-        <Route path="/General" element={ <News key='General'  pageSize={pageSize} country="in" category="General"/>}></Route>
-        <Route path="/Health" element={ <News key='Health'  pageSize={pageSize} country="in" category="Health"/>}></Route>
-        <Route path="/Science" element={ <News key='Science'  pageSize={pageSize} country="in" category="Science"/>}></Route>
-        <Route path="/Sports" element={ <News key='Sports'  pageSize={pageSize} country="in" category="Sports"/>}></Route>
-        <Route path="/Technology" element={ <News  key='Technology}>' pageSize={pageSize} country="in" category="Technology"/>}></Route>
+        <Route path="/" element={< News  key='General' setProgress={setProgress} pageSize={pageSize} country="in" setProgress={setProgress} category="General"/>}></Route>
+        <Route path="/Business" element={ <News key='Business'  setProgress={setProgress} pageSize={pageSize} country="in" category="Business"/>}></Route>
+        <Route path="/Entertainment" element={<News key='Entertainment'  setProgress={setProgress} pageSize={pageSize} country="in" category="Entertainment"/>}></Route>
+        <Route path="/General" element={ <News key='General'  setProgress={setProgress} pageSize={pageSize} country="in" category="General"/>}></Route>
+        <Route path="/Health" element={ <News key='Health'  setProgress={setProgress} pageSize={pageSize} country="in" category="Health"/>}></Route>
+        <Route path="/Science" element={ <News key='Science'  setProgress={setProgress} pageSize={pageSize} country="in" category="Science"/>}></Route>
+        <Route path="/Sports" element={ <News key='Sports'  setProgress={setProgress} pageSize={pageSize} country="in" category="Sports"/>}></Route>
+        <Route path="/Technology" element={ <News  key='Technology}>' setProgress={setProgress} pageSize={pageSize} country="in" category="Technology"/>}></Route>
 
       </Routes>
       </Router>
