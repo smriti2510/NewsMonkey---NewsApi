56e762fff7269edafcb4bbeeca29e8497e58bf4d
Smriti Agarwal <80616769+smriti2510@users.noreply.github.com>
2023-05-24 18:34:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import React, { Component,  } from 'react'
+
+import React, { useState } from 'react'
 import NavBar from './Components/NavBar';
 import News from './Components/News';
-import React, { useState } from 'react'
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
+const App = ()=> {
+  const pageSize = 6;
+  const [ setProgress] = useState(0)
+ 
+  return (
+    <div>
+      <Router>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={< News  key='General' pageSize={pageSize} country="in" setProgress={setProgress} category="General"/>}></Route>
+        <Route path="/Business" element={ <News key='Business'  pageSize={pageSize} country="in" category="Business"/>}></Route>
+        <Route path="/Entertainment" element={<News key='Entertainment'  pageSize={pageSize} country="in" category="Entertainment"/>}></Route>
+        <Route path="/General" element={ <News key='General'  pageSize={pageSize} country="in" category="General"/>}></Route>
+        <Route path="/Health" element={ <News key='Health'  pageSize={pageSize} country="in" category="Health"/>}></Route>
+        <Route path="/Science" element={ <News key='Science'  pageSize={pageSize} country="in" category="Science"/>}></Route>
+        <Route path="/Sports" element={ <News key='Sports'  pageSize={pageSize} country="in" category="Sports"/>}></Route>
+        <Route path="/Technology" element={ <News  key='Technology}>' pageSize={pageSize} country="in" category="Technology"/>}></Route>
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Router>
-        <NavBar/>
-        <Routes>
-          <Route path="/" element={< News  key='General' pageSize={9} country="in" category="General"/>}></Route>
-          <Route path="/Business" element={ <News key='Business'  pageSize={9} country="in" category="Business"/>}></Route>
-          <Route path="/Entertainment" element={<News key='Entertainment'  pageSize={9} country="in" category="Entertainment"/>}></Route>
-          <Route path="/General" element={ <News key='General'  pageSize={9} country="in" category="General"/>}></Route>
-          <Route path="/Health" element={ <News key='Health'  pageSize={9} country="in" category="Health"/>}></Route>
-          <Route path="/Science" element={ <News key='Science'  pageSize={9} country="in" category="Science"/>}></Route>
-          <Route path="/Sports" element={ <News key='Sports'  pageSize={9} country="in" category="Sports"/>}></Route>
-          <Route path="/Technology" element={ <News  key='Technology}>' pageSize={9} country="in" category="Technology"/>}></Route>
-
-        </Routes>
-        </Router>
-      </div>
-    )
-  }
+      </Routes>
+      </Router>
+    </div>
+  )
+ 
 }
 
 export default App;
